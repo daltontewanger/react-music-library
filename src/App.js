c42badcb742503214675c0afe1c4b0b483a21299
Dalton Tewanger <daltontewanger@yahoo.com>
2024-04-09 22:05:23 -0700

    Added react router DOM and artist/album pages to view new components upon selecting them

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useState, useEffect, useRef } from "react";
-import SearchBar from "./Components/SearchBar";
-import Gallery from "./Components/Gallery";
+import { useState, useRef } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { DataContext } from "./context/DataContext";
 import { SearchContext } from "./context/SearchContext";
+import SearchBar from "./Components/SearchBar";
+import Gallery from "./Components/Gallery";
+import ArtistView from "./Components/ArtistView";
+import AlbumView from "./Components/AlbumView";
 
 function App() {
   const [message, setMessage] = useState("Search for Music!");
@@ -28,18 +31,26 @@ function App() {
 
   return (
     <div className="App">
-      <SearchContext.Provider
-        value={{
-          term: searchInput,
-          handleSearch: handleSearch,
-        }}
-      >
-        <SearchBar />
-      </SearchContext.Provider>
       {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <SearchContext.Provider
+          value={{
+            term: searchInput,
+            handleSearch: handleSearch,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<SearchBar />} />
+          </Routes>
+        </SearchContext.Provider>
+        <DataContext.Provider value={data}>
+          <Routes>
+            <Route path="/" element={<Gallery />} />
+            <Route path="/album/:id" element={<AlbumView />} />
+            <Route path="/artist/:id" element={<ArtistView />} />
+          </Routes>
+        </DataContext.Provider>
+      </Router>
     </div>
   );
 }
