f93a862fee3e1d60296180cc17482b3da96eaa00
Dalton Tewanger <daltontewanger@yahoo.com>
2024-04-10 21:41:29 -0700

    Added album and artist view compenents working with proxy

--- src/Components/AlbumView.js
+++ src/Components/AlbumView.js
@@ -1,14 +1,47 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 function AlbumView() {
     const { id } = useParams()
+    const navigate = useNavigate()
     const [ albumData, setAlbumData ] = useState([])
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`;
+        const fetchData = async () => {
+          const response = await fetch(API_URL);
+          const resData = await response.json();
+          console.log(resData)
+          setAlbumData(resData.results);
+        };
+        fetchData();
+      }, [id]);
+
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {/* <h2>The id passed was: {id}</h2> */}
+            {albumData.length > 0 ? <h2>{albumData[0].collectionName}</h2> : <h2>Loading...</h2>}
+            {navButtons()}
+            {renderSongs}
         </div>
     )
 }
